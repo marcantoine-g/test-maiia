919fb7c9f87a86224d75ede6ccd74219d0a0e102
marcantoine-g <marc-antoine.g@outlook.com>
2021-01-14 20:33:13 +0100

    retrait de la propriété key dans l'item (mise en avant)

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Item.css";
 
 function Item(props) {
-  const { srcImg, title, id } = props;
+  const { srcImg, title } = props;
 
   return (
-    <div className="item" key={id}>
+    <div className="item">
       <img className="itemImage" src={srcImg} alt={title} />
       <p className="itemTitle">{title}</p>
     </div>
