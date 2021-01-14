d3d1beeaf695e28e857d5e9ada8c7c917ff33d92
marcantoine-g <marc-antoine.g@outlook.com>
2021-01-14 14:51:22 +0100

    Création d'un Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
+import Item from "./components/Item/Item";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>hello world</h1>
+        <h1>Test technique Maiia</h1>
       </header>
+      <section className="homePage">{/* <ListItems /> */}</section>
+      <Item title="test" srcImg="test" />
     </div>
   );
 }
