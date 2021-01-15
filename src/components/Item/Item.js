8f0e749dd2e7fceef4efc8556d29f0e93b8548ee
marcantoine-g <marc-antoine.g@outlook.com>
2021-01-15 11:39:39 +0100

    Changement de bouton selon qu'on soit dans le panier ou non

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,22 +1,31 @@
 import React from "react";
 import "./Item.css";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { cartSlice } from "../../features/CartSlice";
 
 function Item(props) {
   const dispatch = useDispatch();
-  const { srcImg, title, id } = props;
-
-  const addInCart = (e) => {
-    console.log(srcImg, title, id);
-    dispatch(cartSlice.actions.addProduct({ id, title, srcImg }));
-  };
+  const { srcImg, title, id, inCart } = props;
 
   return (
     <div className="item">
       <img className="itemImage" src={srcImg} alt={title} />
       <p className="itemTitle">{title}</p>
-      <button onClick={() => addInCart()}>add</button>
+      {inCart ? (
+        <button
+          onClick={() => dispatch(cartSlice.actions.removeProduct({ id }))}
+        >
+          remove
+        </button>
+      ) : (
+        <button
+          onClick={() =>
+            dispatch(cartSlice.actions.addProduct({ id, title, srcImg }))
+          }
+        >
+          add
+        </button>
+      )}
     </div>
   );
 }
